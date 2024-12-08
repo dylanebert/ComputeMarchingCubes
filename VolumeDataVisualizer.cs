fc7b40fd75ea7e3d772bcb6a83a97e709fed7e1e
dylan <dylan@huggingface.co>
2024-12-07 23:08:01 -0500

    .

--- VolumeDataVisualizer.cs
+++ VolumeDataVisualizer.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using UnityEngine.UIElements;
-using Unity.Properties;
 
 namespace MarchingCubes {
     public sealed class VolumeDataVisualizer : MonoBehaviour {
@@ -8,16 +6,14 @@ namespace MarchingCubes {
         [SerializeField] private Vector3Int _dimensions = new(64, 64, 64);
         [SerializeField] private float _gridScale = 1.0f / 64;
         [SerializeField] private int _triangleBudget = 65536 * 16;
-
         [SerializeField] private ComputeShader _builderCompute = null;
-
-        [CreateProperty] public float TargetValue { get; set; } = 0.4f;
-        private float _builtTargetValue;
-
-        private int VoxelCount => _dimensions.x * _dimensions.y * _dimensions.z;
+        [SerializeField] private float _targetValue = 0.4f;
 
         private ComputeBuffer _voxelBuffer;
         private MeshBuilder _builder;
+        private float _builtTargetValue;
+
+        private int VoxelCount => _dimensions.x * _dimensions.y * _dimensions.z;
 
         private void Start() {
             _voxelBuffer = new ComputeBuffer(VoxelCount, sizeof(float));
@@ -26,9 +22,6 @@ namespace MarchingCubes {
             float[] voxelData = new float[VoxelCount];
             System.Buffer.BlockCopy(_volumeData.bytes, 0, voxelData, 0, voxelData.Length * sizeof(float));
             _voxelBuffer.SetData(voxelData);
-
-            // UI data source
-            FindFirstObjectByType<UIDocument>().rootVisualElement.dataSource = this;
         }
 
         private void OnDestroy() {
@@ -37,13 +30,12 @@ namespace MarchingCubes {
         }
 
         private void Update() {
-            // Rebuild the isosurface only when the target value has been changed.
-            if (TargetValue == _builtTargetValue) return;
+            if (_targetValue == _builtTargetValue) return;
 
-            _builder.BuildIsosurface(_voxelBuffer, TargetValue, _gridScale);
+            _builder.BuildIsosurface(_voxelBuffer, _targetValue, _gridScale);
             GetComponent<MeshFilter>().sharedMesh = _builder.Mesh;
 
-            _builtTargetValue = TargetValue;
+            _builtTargetValue = _targetValue;
         }
 
         private void OnDrawGizmos() {
