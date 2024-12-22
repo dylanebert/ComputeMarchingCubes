2915115499c1f54aec5bdf74bb12b456302868d5
dylan <dylan@huggingface.co>
2024-12-22 17:20:50 -0500

    .

--- Constants.cs
+++ Constants.cs
@@ -1,6 +1,6 @@
 namespace MarchingCubes {
     public static class Constants {
-        public const int ChunkSize = 64;
+        public const int ChunkSize = 16;
         public const float GridScale = 1f;
     }
 }
